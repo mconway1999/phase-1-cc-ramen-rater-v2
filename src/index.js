cda88ecab865dccce8e397e8150a8232cc6e2be0
Madeline Conway <conway.maddy@yahoo.com>
2024-09-03 14:19:34 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,26 +1,84 @@
-// index.js
-
-// Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  console.log(ramen)
+  const displayImage = document.querySelector(".detail-image")
+  displayImage.src = ramen.image
+
+  const ramenName = document.querySelector('.name')
+  ramenName.textContent = ramen.name
+
+  const commentDisplay = document.querySelector('.restaurant')
+  commentDisplay.textContent = ramen.restaurant
+
+  const insertRating = document.getElementById('rating-display')
+  insertRating.textContent = ramen.rating
+
+  const insertComment = document.getElementById('comment-display')
+  insertComment.textContent = ramen.comment
+
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const addSubmitListenerForm = document.getElementById ('new-ramen')
+  addSubmitListenerForm.addEventListener ('submit', (event) => {
+    event.preventDefault()
+    console.log(event)
+
+  const addNewRestaurant = document.getElementById ('new-restaurant')
+  const addNewName = document.getElementById('new-name') 
+  const addNewImage = document.getElementById ('new-image')
+  const addNewRating = document.getElementById ('new-rating')
+  const addNewComment = document.getElementById ('new-comment')
+
+    const newRamen = {
+      name: addNewName.value,
+      restaurant: addNewRestaurant.value,
+      image: addNewImage.value,
+      rating: addNewRating.value,
+      comment: addNewComment.value
+    }
+    console.log(newRamen)
+    const ramenMenuDiv = document.getElementById('ramen-menu');
+    const foodImage = document.createElement('img')
+    foodImage.src = newRamen.image
+    ramenMenuDiv.appendChild(foodImage)
+    foodImage.addEventListener('click', () => {
+      handleClick(newRamen)
+    })
+  })
+
 }
 
 const displayRamens = () => {
-  // Add code
+  const ramenMenuDiv = document.getElementById('ramen-menu');
+  fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(ramen => {
+        const foodImage = document.createElement('img')
+        foodImage.src = ramen.image
+        ramenMenuDiv.appendChild(foodImage)
+        foodImage.addEventListener('click', () => {
+          handleClick(ramen)
+        })
+      })
+    })
+
+
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  document.addEventListener('DOMContentLoaded', () => {
+   displayRamens ()
+   addRamenNamesToMenu ()
+  addToCart ()
+  addSubmitListener()
+  })
+
 }
 
 main()
 
-// Export functions for testing
+
 export {
   displayRamens,
   addSubmitListener,
